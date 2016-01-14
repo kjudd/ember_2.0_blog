f748fc30de5b0f1a2422f5d22ce28815481400d5
kjudd <vedette.kristen@gmail.com>
2016-01-13 19:57:26 -0800

    fixed model to display post text and timestamp

--- app/models/post.js
+++ app/models/post.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  author: DS.attr('string')
+  author: DS.attr('string'),
+  createdDate: DS.attr('date'),
+  text: DS.attr('string')
 });
